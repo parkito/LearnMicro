c19fbad15396ddc46fb3b1af718c0aaa0dd25428
parkito <artyom-karnov@yandex.ru>
2018-04-04 19:17:26 +0300

    Adapt microservices to config and discovery service

--- SERVICE_DISCOVERY/src/main/java/com/parkito/learnmicro/eureka/DiscoveryServiceApplication.java
+++ SERVICE_DISCOVERY/src/main/java/com/parkito/learnmicro/eureka/DiscoveryServiceApplication.java
@@ -2,8 +2,9 @@ package com.parkito.learnmicro.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
--- USER_SERVICE/src/main/kotlin/com/parkito/learnmicro/user/service/UserApplication.kt
+++ USER_SERVICE/src/main/kotlin/com/parkito/learnmicro/user/service/UserApplication.kt
@@ -1,10 +1,10 @@
 package com.parkito.learnmicro.user.service;
 
 import com.google.common.base.Predicates
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -14,9 +14,10 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
-@EnableSwagger2
-@EnableDiscoveryClient
 @Configuration
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
 class UserApplication {
 
